514968ad8e4c5914ec37e3d25eeeee1fdfdb584a
Matthew Butt <matthew.butt@theguardian.com>
2015-12-04 15:19:00 +0000

    Remove multiple redundant references to $WORKING_DIR

--- test.sh
+++ test.sh
@@ -47,7 +47,7 @@ test_the_script_does_not_alert_when_no_files_have_been_changed()
 
 test_the_script_displays_an_alert_when_a_new_file_containing_an_aws_key_is_committed()
 {
-    echo 'foo.aws.key="'$SAMPLE_AWS_KEY'"' > $WORKING_DIR/new.conf
+    echo 'foo.aws.key="'$SAMPLE_AWS_KEY'"' > new.conf
     git add -A
 
     git commit -m "This commit should fail" 2>&1 | assertPattern "$CREDENTIALS_FOUND"
@@ -55,11 +55,11 @@ test_the_script_displays_an_alert_when_a_new_file_containing_an_aws_key_is_commi
 
 test_the_script_displays_an_alert_when_an_aws_key_is_added_to_an_existing_file()
 {
-    echo "# No secrets in here" > $WORKING_DIR/existing.conf
+    echo "# No secrets in here" > existing.conf
     git add -A
     git commit -m "Preliminary commit" &> /dev/null
 
-    echo 'foo.aws.key="'$SAMPLE_AWS_KEY'"' >> $WORKING_DIR/existing.conf   
+    echo 'foo.aws.key="'$SAMPLE_AWS_KEY'"' >> existing.conf   
     git add -A
 
     git commit -m "This commit should fail" 2>&1 | assertPattern "$CREDENTIALS_FOUND"
@@ -67,7 +67,7 @@ test_the_script_displays_an_alert_when_an_aws_key_is_added_to_an_existing_file()
 
 test_the_script_does_not_alert_when_a_new_file_containing_no_aws_keys_are_committed()
 {
-    echo "# No secrets in here" > $WORKING_DIR/existing.conf
+    echo "# No secrets in here" > existing.conf
     git add -A
 
     message="This commit should succeed"
@@ -81,18 +81,18 @@ test_findFilesChanged_returns_nothing_when_no_files_are_changed()
 
 test_findFilesChanged_finds_a_new_file()
 {
-    echo "# I am a new file" > $WORKING_DIR/new.conf
+    echo "# I am a new file" > new.conf
     git add -A
     findFilesChanged | assertPattern "new.conf"
 }
 
 test_findFilesChanged_finds_more_than_one_file()
 {
-    echo "# I am a new file" > $WORKING_DIR/new.conf
+    echo "# I am a new file" > new.conf
     git add -A
     git commit -m "Preliminary commit" &> /dev/null
-    echo "# I now have some changes" >> $WORKING_DIR/new.conf
-    echo "# I am a new file" > $WORKING_DIR/new2.conf
+    echo "# I now have some changes" >> new.conf
+    echo "# I am a new file" > new2.conf
     git add -A
 
     findFilesChanged | (
@@ -105,11 +105,11 @@ test_findFilesChanged_finds_more_than_one_file()
 
 test_findFilesChanged_finds_a_modified_file()
 {
-    echo "# I am an existing file" > $WORKING_DIR/existing.conf
+    echo "# I am an existing file" > existing.conf
     git add -A
     git commit -m "Preliminary commit" &> /dev/null
 
-    echo "# I now have some changes" >> $WORKING_DIR/existing.conf
+    echo "# I now have some changes" >> existing.conf
     git add -A
     findFilesChanged | assertPattern "existing.conf"
 }
@@ -122,130 +122,130 @@ test_findChanges_finds_nothing_when_no_files_are_changed()
 test_findChanges_finds_key_surrounded_by_double_quotes()
 {
     content='foo.aws.key="'$SAMPLE_AWS_KEY'"'
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_key_surrounded_by_single_quotes()
 {
     content="foo.aws.key='$SAMPLE_AWS_KEY'"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_key_surrounded_by_whitespace()
 {
     content="foo.aws.key= $SAMPLE_AWS_KEY #comment to prevent whitespace collapse"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_key_in_undelimited_bash_format()
 {
     content="foo.aws.key=$SAMPLE_AWS_KEY"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_key_in_undelimited_yaml_format()
 {
     content="foo.aws.key: $SAMPLE_AWS_KEY"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_key_alone_on_line()
 {
     content="$SAMPLE_AWS_KEY"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_surrounded_by_double_quotes()
 {
     content='foo.aws.secret="'$SAMPLE_AWS_SECRET'"'
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_surrounded_by_single_quotes()
 {
     content="foo.aws.secret='$SAMPLE_AWS_SECRET'"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_surrounded_by_whitespace()
 {
     content="foo.aws.secret= $SAMPLE_AWS_SECRET #comment to prevent whitespace collapse"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_in_undelimited_bash_format()
 {
     content="foo.aws.secret=$SAMPLE_AWS_SECRET"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_in_undelimited_yaml_format()
 {
     content="foo.aws.secret: $SAMPLE_AWS_SECRET"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_alone_on_line()
 {
     content="$SAMPLE_AWS_SECRET"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_with_special_characters_surrounded_by_double_quotes()
 {
     content='foo.aws.secret="'$ANOTHER_SAMPLE_AWS_SECRET'"'
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_with_special_characters_surrounded_by_single_quotes()
 {
     content="foo.aws.secret='$ANOTHER_SAMPLE_AWS_SECRET'"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_with_special_characters_surrounded_by_whitespace()
 {
     content="foo.aws.secret= $ANOTHER_SAMPLE_AWS_SECRET #comment to prevent whitespace collapse"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_with_special_characters_in_undelimited_yaml_format()
 {
     content="foo.aws.secret: $ANOTHER_SAMPLE_AWS_SECRET"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_secret_with_special_characters_alone_on_line()
 {
     content="$ANOTHER_SAMPLE_AWS_SECRET"
-    echo "$content" > $WORKING_DIR/new.conf
-    echo "$WORKING_DIR/new.conf" | findChanges | assertPattern "$content"
+    echo "$content" > new.conf
+    echo "new.conf" | findChanges | assertPattern "$content"
 }
 
 test_findChanges_finds_credential_in_second_file()
 {
-    echo "# Nothing to see here" > $WORKING_DIR/old.conf
+    echo "# Nothing to see here" > old.conf
     content="$SAMPLE_AWS_SECRET"
-    echo "$content" > $WORKING_DIR/new.conf
+    echo "$content" > new.conf
     (
-        echo "$WORKING_DIR/old.conf"
-        echo "$WORKING_DIR/new.conf"
+        echo "old.conf"
+        echo "new.conf"
     ) | findChanges | assertPattern "$content"
 }
 
